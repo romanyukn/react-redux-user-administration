b53cd30f168e150abbfef11638e3ca82a49db776
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-12-09 19:37:21 +1100

    add button for new user

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,35 +1,43 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from "react-router-dom";
 import UserListItem from './UserListItem';
 
 function UsersList() {
     const users = useSelector(state => state);
-    console.log(users);
+    const history = useHistory();
+
+    function goToAddUser() {
+        history.push("/add-user");
+    }
     
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                <th scope="col">№</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Password</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map((el) => {
-                    <UserListItem 
-                        key={el.id}
-                        id={el.id}
-                        firstName={el.firstName}
-                        lastName={el.lastName}
-                        email={el.email}
-                        password={el.password}
-                    />
-                })}
-            </tbody>
-        </table>
+        <div className="container">
+            <button type="button" class="btn btn-primary mb-3 mt-3" onClick={goToAddUser}>Add new user</button>
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">№</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((el) => {
+                        return <UserListItem 
+                            key={el.id}
+                            id={el.id}
+                            firstName={el.firstName}
+                            lastName={el.lastName}
+                            email={el.email}
+                            password={el.password}
+                        />
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
