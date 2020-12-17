fa1de5f827c8fe5704519822c58670e5120ed8bf
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-12-17 18:34:20 +1100

    fix password field

--- src/components/AddUserFormField.jsx
+++ src/components/AddUserFormField.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 function AddUserFormField(props) {
     const {name, value, isValid, className, error, onChange, onFocus, changeMode} = props;
+    function  fieldType() {
+        if (name === "password") return ("password");
+        return ("text")
+    }
     return(
         <div className="form-group">
             <input
-                type="text"
+                type={fieldType()}
                 className={className}
                 id="formGroupExampleInput"
                 name={name}
--- src/components/UserListItem.jsx
+++ src/components/UserListItem.jsx
@@ -9,7 +9,6 @@ function UserListItem(props) {
             <td>{firstName}</td>
             <td>{lastName}</td>
             <td>{email}</td>
-            <td type="password">{password}</td>
             <button type="button" className="btn btn-primary mr-2 ml-2 mt-2" onClick={() => onViewDetails(id)}>Details</button>
             <button type="button" className="btn btn-danger mt-2" onClick={() => onDelete(id)}>Delete</button>
         </tr>
